9c4c34a6e864cd69a33e50dbfe4494ffd412474b
TCKing <taylorcaudle@gmail.com>
2023-04-30 00:29:13 -0700

    refined hair Shader Graphs, added gradients, runtime value functions

--- Assets/_MasterGameAssets/Scripts/HairDemoSwitcher.cs
+++ Assets/_MasterGameAssets/Scripts/HairDemoSwitcher.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.DemoTeam.Hair;
+using UnityEngine.UI;
 
 public class HairDemoSwitcher : MonoBehaviour
 {
+    public Slider StrandDiameter;
+    public Slider StrandMargin;
+    public Slider Stiffness;
+    public Slider Dampening;
+    public Slider AngularDampening;
+    public Slider Gravity;
+    public Slider GlobalPosition;
+    [Space(10)]
 
     public GameObject[] HairObjects;
     public Material[] HairMaterials;
@@ -13,13 +22,11 @@ public class HairDemoSwitcher : MonoBehaviour
     internal int HairIndex;
     internal int HairMatIndex;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Tab))
@@ -61,6 +68,64 @@ public class HairDemoSwitcher : MonoBehaviour
             }
         }
 
-
     }
+
+
+    public void SetStrandDiameter()
+    {
+        for (int i = 0; i < HairObjects.Length; i++)
+        {
+            var HairInst = HairObjects[i].GetComponent<HairInstance>();
+            HairInst.strandGroupDefaults.settingsStrands.strandDiameter = StrandDiameter.value;
+        }
+    }
+    public void SetStrandMargin()
+    {
+        for (int i = 0; i < HairObjects.Length; i++)
+        {
+            var HairInst = HairObjects[i].GetComponent<HairInstance>();
+            HairInst.strandGroupDefaults.settingsStrands.strandMargin = StrandMargin.value;
+        }
+    }
+    public void SetStiffness()
+    {
+        for (int i = 0; i < HairObjects.Length; i++)
+        {
+            var HairInst = HairObjects[i].GetComponent<HairInstance>();
+            HairInst.strandGroupDefaults.settingsSolver.stiffness = Stiffness.value;
+        }
+    }
+    public void SetDampening()
+    {
+        for (int i = 0; i < HairObjects.Length; i++)
+        {
+            var HairInst = HairObjects[i].GetComponent<HairInstance>();
+            HairInst.strandGroupDefaults.settingsSolver.dampingFactor = Dampening.value;
+        }
+    }
+    public void SetAngularDampening()
+    {
+        for (int i = 0; i < HairObjects.Length; i++)
+        {
+            var HairInst = HairObjects[i].GetComponent<HairInstance>();
+            HairInst.strandGroupDefaults.settingsSolver.angularDampingFactor = AngularDampening.value;
+        }
+    }
+    public void SetGravity()
+    {
+        for (int i = 0; i < HairObjects.Length; i++)
+        {
+            var HairInst = HairObjects[i].GetComponent<HairInstance>();
+            HairInst.strandGroupDefaults.settingsSolver.gravity = Gravity.value;
+        }
+    }
+    public void SetGlobalPosition()
+    {
+        for (int i = 0; i < HairObjects.Length; i++)
+        {
+            var HairInst = HairObjects[i].GetComponent<HairInstance>();
+            HairInst.strandGroupDefaults.settingsSolver.globalPositionInfluence = GlobalPosition.value;
+        } 
+    }
+
 }
